c1d1cb5da05f55236b5d0636dc65f7ba4566f81e
Sutenresu <joaovitor_g.s@hotmail.com>
2020-04-11 00:10:53 -0300

    Promotion and correction in .gitignore

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,22 +20,31 @@ public class Program {
 		
 		while(!chessMatch.getCheckMate()) {
 			try {
-			UI.clearScreen();
-			UI.printMatch(chessMatch,captured);
-			System.out.print("\nSource: ");
-			ChessPosition source = UI.readChessPosition(sc);
-		
-			boolean[][] possibleMoves = chessMatch.possibleMoves(source);
-			UI.clearScreen();
-			UI.printBoard(chessMatch.getPieces(),possibleMoves);
+				UI.clearScreen();
+				UI.printMatch(chessMatch,captured);
+				System.out.print("\nSource: ");
+				ChessPosition source = UI.readChessPosition(sc);
 			
-			System.out.print("\nTarget: ");
-			ChessPosition target = UI.readChessPosition(sc);
-
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-			
-			if(capturedPiece != null)
-				captured.add(capturedPiece);
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(),possibleMoves);
+				
+				System.out.print("\nTarget: ");
+				ChessPosition target = UI.readChessPosition(sc);
+	
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				
+				if(capturedPiece != null)
+					captured.add(capturedPiece);
+				if(chessMatch.getPromoted()!=null) {
+					System.out.print("Enter piece for promotion (B/H/R/Q): ");
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B")&&!type.equals("H")&&!type.equals("R")&&!type.equals("Q")) {
+						System.out.print("Invalid value! Enter piece for promotion (B/H/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
